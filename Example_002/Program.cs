3dcc02aa759d2acac6231c2023197303394be3d1
gaazoo <gaazoo@yandex.ru>
2023-07-04 21:56:57 +0700

    Gitignore

---
+++ Example_002/Program.cs
@@ -0,0 +1,9 @@
+﻿ Console.WriteLine("Введите первое число:");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите второе число:");
+ int b = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите третье число:");
+ int c = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(a*a);
+ Console.WriteLine(b*b);
+ Console.WriteLine(c*c);
--- example_001/Program.cs
+++ example_001/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World, Bitch!");
+﻿ // Console.WriteLine("Введите первое число:");
+ // int a = Convert.ToInt32(Console.ReadLine());
+ // Console.WriteLine("Введите второе число:");
+ // int b = Convert.ToInt32(Console.ReadLine());
+ // Console.WriteLine("Введите третье число:");
+ // int c = Convert.ToInt32(Console.ReadLine());
+ // Console.WriteLine(a*a);
+ // Console.WriteLine(b*b);
+// Console.WriteLine(c*c); 
+Console.WriteLine("Введите номер дня недели:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+    Console.WriteLine("День недели - понедельник");
+else if (a==2)
+Console.WriteLine("День недели - вторник");
+else if (a==3)
+Console.WriteLine("День недели - среда");
+else if (a==4)
+Console.WriteLine("День недели - четверг");
+else if (a==5)
+Console.WriteLine("День недели - пятница");
+else if (a==6)
+Console.WriteLine("День недели - суббота");
+else if (a==7)
+Console.WriteLine("День недели - воскресенье");
+else Console.WriteLine("Такого дня недели не существует");
